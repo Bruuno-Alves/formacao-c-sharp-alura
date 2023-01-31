8c1c79965875ef362b43d4883093b2118536474c
Bruuno-Alves <bruno.a.santos@live.com>
2023-01-31 20:42:23 -0300

    Conclusao da formação

--- csharp_exception-main/csharp_exception/Program.cs
+++ csharp_exception-main/csharp_exception/Program.cs
@@ -1,16 +1,54 @@
 ﻿using csharp_exception.Titular;
 using csharp_exception.Contas;
+using csharp_exception;
 
 
+
+using(LeitorDeArquivo leitor = new LeitorDeArquivo("contas.txt"))
+{
+    leitor.LerProximaLinha();
+    leitor.LerProximaLinha();
+    leitor.LerProximaLinha();
+}
+
+/*
 try
 {
-    ContaCorrente conta1 = new ContaCorrente(0, "1234-X");
+    leitor.LerProximaLinha();
+    leitor.LerProximaLinha();
+}
+catch (IOException ex)
+{
+    Console.WriteLine("Leitura de arquivo interrompida");
+}
+finally
+{
+    leitor.Fechar();
+}
+*/
+
+/*
+try
+{
+    ContaCorrente conta1 = new ContaCorrente(2, "1234-X");
+    
+    conta1.Sacar(50);
+    Console.WriteLine(conta1.GetSaldo());
+    conta1.Sacar(150);
+    Console.WriteLine(conta1.GetSaldo());
+    
 }
 catch(ArgumentException ex)
 {
-    Console.WriteLine("Parâmetro com erro" + ex.ParamName);
+    Console.WriteLine($"Parâmetro com erro {ex.ParamName}");
     Console.WriteLine("Não é possível criar uma conta com o número de agência menor ou igual a zero!");
+    Console.WriteLine(ex.StackTrace);
     Console.WriteLine(ex.Message);
-    
 }
+catch(SaldoInsuficienteException ex)
+{
+    Console.WriteLine("Operação Negada! Saldo insuficiente");
+    Console.WriteLine(ex.Message);
+}
+*/
 
